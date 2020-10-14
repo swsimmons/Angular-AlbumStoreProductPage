e8577be921e960b73a87fc7af7fb49dad5b79e27
Sean Simmons <sean.simmons@hpfm.com>
2020-10-14 10:59:58 -0500

    Service injection

---
+++ src/app/product.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http'
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+import { ErrorObservable } from 'rxjs/observable/ErrorObservable';
+
+@Injectable()
+export class ProductService {
+  private _albumUrl = '../assets/album.json';
+
+  constructor(private _http: Http) { }
+
+  getAlbum (id: number): Observable<Response> {
+    return this._http.get(this._albumUrl).map(
+      response => response.json()
+    );
+  }
+
+}
