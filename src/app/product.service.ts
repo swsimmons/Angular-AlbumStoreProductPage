d67c9fbe9e2d723930e1e74bc749726735a65ccd
Sean Simmons <sean.simmons@hpfm.com>
2020-10-14 11:15:51 -0500

    Album and Track interface utilization

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -3,6 +3,7 @@ import { Http, Response } from '@angular/http'
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 import { ErrorObservable } from 'rxjs/observable/ErrorObservable';
+import { Album } from './album';
 
 @Injectable()
 export class ProductService {
@@ -10,9 +11,10 @@ export class ProductService {
 
   constructor(private _http: Http) { }
 
-  getAlbum (id: number) {
+  getAlbum (id: number): Observable<Album> {
     return this._http.get(this._albumUrl).map(
-      response => response.json()
+      (response) => <Album>response.json(),
+      (error: any) => new ErrorObservable(error)
     );
   }
 
